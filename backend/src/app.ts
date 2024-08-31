dffe20a304564af14f01acd4a48a4ea247fc24f8
brunfernandez98 <brunfernandezrod@hotmail.com>
2024-08-30 22:46:34 -0300

    chore(env): configure BASE_URL in .env

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -3,10 +3,11 @@ import express from "express"
 import productRoutes from "@routes/productRoute"
 
 const app = express()
+const baseURL = process.env.BASE_URL ?? "/api"
 
 app.use(express.json())
 
-app.use("./api", productRoutes)
+app.use(baseURL, productRoutes)
 /* app.use(errorHanlder) */
 
 export default app
